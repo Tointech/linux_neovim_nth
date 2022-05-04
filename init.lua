9fabb8b6929275695b87bb36b7c7e640216f47fc
dev.tronghieu <dev.ngtronghieu@gmail.com>
2022-05-04 15:27:53 +0700

    LSP saga

--- init.lua
+++ init.lua
@@ -1,8 +1,15 @@
+-- General modules
 require('settings')
 require('mappings')
-require('colorschemes-config.nightfox')
 require('packer-config')
+
+-- Themes
+require('colorschemes-config.nightfox')
+
+-- File browser
 require('nvim-tree-config')
+
+-- LSP
 require('lsp-config.language-servers')
 require('lsp-config.lsp-installer')
 require('lsp-config.nvim-cmp')
