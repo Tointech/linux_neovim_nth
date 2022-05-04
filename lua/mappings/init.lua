82a21bd0d66b3f5c11dd04bf26f68964c7c08aef
dev.tronghieu <dev.ngtronghieu@gmail.com>
2022-05-04 16:05:54 +0700

    Update key mappings

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -1,6 +1,23 @@
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
+local opts_nore = { noremap = false, silent = true }
 
 vim.g.mapleader = ' '
 
+-- Toggle NvimTree
 map('n', '<F2>', ':NvimTreeToggle<CR>', opts)
+
+-- Select all
+map('n', '<C-a>', 'gg<S-v>G', opts_nore)
+
+-- Split window 
+map('n', 'hs', ':split<Return><C-w>w', opts_nore)
+map('n', 'vs', ':vsplit<Return><C-w>w', opts_nore)
+map('n', '<leader>h', '<C-w>h', opts)
+map('n', '<leader>j', '<C-w>j', opts)
+map('n', '<leader>k', '<C-w>k', opts)
+map('n', '<leader>l', '<C-w>l', opts)
+map('n', '<leader><left>', '<C-w>h', opts)
+map('n', '<leader><down>', '<C-w>j', opts)
+map('n', '<leader><up>', '<C-w>k', opts)
+map('n', '<leader><right>', '<C-w>l', opts)
